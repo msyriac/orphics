e3d22d5d1815470c589d5810773157313d3a8237
Mathew Madhavacheril <mathewsyriac@gmail.com>
2016-11-12 11:59:00 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='orphics',
+      version='0.1',
+      description='Cosmology Analysis',
+      url='https://github.com/msyriac/orphics',
+      author='Mathew Madhavacheril',
+      author_email='mathewsyriac@gmail.com',
+      license='BSD-2-Clause',
+      packages=['orphics'],
+      zip_safe=False)
