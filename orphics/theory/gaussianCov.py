64004de952f2a0090a5659e9d17f4d9878748cf5
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-06-08 16:48:19 -0400

    bin theory for SN

--- orphics/theory/gaussianCov.py
+++ orphics/theory/gaussianCov.py
@@ -80,19 +80,24 @@ class LensForecast:
         if Nls is not None: self.Nls[specType] = interp1d(ellsNls,Nls,bounds_error=False,fill_value=np.inf)
         self.theory.loadGenericCls(ellsCls,Cls,specType)
         
-        
+    def _bin_cls(spec,ell_left,ell_right,noise=True):
+        a,b = spec
+        ells = np.arange(ell_left,ell_right+1,1)
+        cls = self.theory.gCl(spec,ells)
+        if noise:
+            if a==b:
+                Noise = self.Nls[spec](ells)
+            else:
+                Noise = 0.
+        tot = cls+Noise
+        return np.sum(ells*tot)/np.sum(ells)
 
     def KnoxCov(self,specTypeXY,specTypeWZ,ellBinEdges,fsky):
         '''
         returns cov(Cl_XY,Cl_WZ),signalToNoise(Cl_XY)^2, signalToNoise(Cl_WZ)^2
         '''
-        def ClTot(spec,ell):
-            a,b = spec
-            if a==b:
-                Noise = self.Nls[spec](ell)
-            else:
-                Noise = 0.
-            return self.theory.gCl(spec,ell)+Noise        
+        def ClTot(spec,ell1,ell2):
+            return self._bin_cls(spec,ell1,ell2,noise=True)
         
         X, Y = specTypeXY
         W, Z = specTypeWZ
@@ -103,12 +108,16 @@ class LensForecast:
         covs = []
         sigs1 = []
         sigs2 = []
-        for ellMid,ellWidth in zip(ellMids,ellWidths):
-            ClSum = ClTot(X+W,ellMid)*ClTot(Y+Z,ellMid)+ClTot(X+Z,ellMid)*ClTot(Y+W,ellMid)
+
+        for ell_left,ell_right in zip(ellBinEdges[:-1],ellBinEdges[1:]):
+            ClSum = ClTot(X+W,ell_left,ell_right)*ClTot(Y+Z,ell_left,ell_right)+ClTot(X+Z,ell_left,ell_right)*ClTot(Y+W,ell_left,ell_right)
+            ellMid = (ell_right+ell_left)/2.
+            ellWidth = ell_right-ell_left
             var = ClSum/(2.*ellMid+1.)/ellWidth/fsky
             covs.append(var)
-            sigs1.append(self.theory.gCl(specTypeXY,ellMid)**2.*np.nan_to_num(1./var))
-            sigs2.append(self.theory.gCl(specTypeWZ,ellMid)**2.*np.nan_to_num(1./var))
+            sigs1.append(self._bin_cls(specTypeXY,ell_left,ell_right,noise=False)**2.*np.nan_to_num(1./var))
+            sigs2.append(self._bin_cls(specTypeWZ,ell_left,ell_right,noise=False)**2.*np.nan_to_num(1./var))
+        
 
         return np.array(covs), np.array(sigs1), np.array(sigs2)
 
