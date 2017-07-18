1015caa4ce5b7135cc86e7a2dfc3aac4015b87d5
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-07-18 18:34:03 -0400

    python 2 and 3 compatible bin optimization

--- orphics/tools/catalogs.py
+++ orphics/tools/catalogs.py
@@ -1,5 +1,75 @@
-
+from __future__ import print_function
 import numpy as np
+
+
+def split_samples(in_samples,split_points):
+    """ Calculate statistics on splits of a sample of data.
+    If in_samples is a list containing measurements of (say) richness
+    and split_points are the richness values corresponding to bin 
+    edges of the splits, this function will return the "S/N", 
+    average richness and number of objects in each bin defined 
+    by the split_points bin edges.
+
+    Here, S/N is average richness times square root of number
+    objects in that bin. If the real S/N is linear in richness
+    and sqrt(N), this returned quantity should be proportional
+    to it.
+
+
+    e.g. 
+    >>> in_samples = [5,1,6,7,2,9,10]
+    >>> split_points = [1,5,10]
+    >>> sns, means, Ns = split_points(in_samples,split_points)
+    >>> print (Ns)
+    >>> [3,4]
+    
+
+    """
+    assert np.all(np.diff(split_points))>0., "Split points should be monotonically increasing."
+    A = np.asarray(in_samples)
+    sns = []
+    means = []
+    Ns = []
+        
+    for a,b in zip(split_points[:-1],split_points[1:]):
+        loc = np.where(np.logical_and(A>a,A<=b))
+        split_mean = A[loc].mean()
+        means.append(split_mean)
+        split_N = len(A[loc])
+        Ns.append(split_N)
+        sns.append(split_mean*np.sqrt(split_N))
+        
+        
+    return np.asarray(sns),np.asarray(means),np.asarray(Ns)
+
+
+def optimize_splits(in_samples,in_splits):
+    """ Given measurements in_samples and bin edges in_splits,
+    this function solves for a new set of bin edges out_splits
+    (keeping the leftmost and rightmost edge fixed) such
+    that the "S/N" in each bin is as close to each other as
+    possible. (Concretely, it minimizes the variance of the
+    S/N in each bin).
+
+    See split_samples for a definition of S/N.
+
+    """
+
+    def cost(*kwargs):
+        in_array = np.asarray(kwargs).flatten()
+        if np.any(np.diff(in_array))<0.: return np.inf
+        out_splits = np.append(np.append(in_splits[0],in_array),in_splits[-1]).flatten()
+        sns,means,Ns = split_samples(in_samples,out_splits)
+        return np.var(sns)
+
+
+    from scipy.optimize import fmin
+    in_splits = np.asarray(in_splits)
+    res = fmin(cost,in_splits[1:-1])
+    out_splits = np.append(np.append(in_splits[0],res),in_splits[-1]).flatten()
+    return out_splits
+    
+
 
 def generateRandomCatalogFromLiteMap(templateLiteMap,N=10000):
 
@@ -62,7 +132,7 @@ def getCountMapsFromCatalogFile(templateLiteMap,fitsFile,raMin,raMax,nsplits=1,N
             fmapper.increment("ct"+str(j%nsplits),'',1)
             j+=1
         i+=1
-        if i%10000==0: print i
+        if i%10000==0: print (i)
 
     f.close()
     return [fmapper.getMap("ct"+str(k),"temp",'') for k in range(nsplits)]
---
+++ tests/testBinOptimization.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+import numpy as np
+import orphics.tools.io as io
+import matplotlib.pyplot as plt
+import sys 
+from orphics.tools.catalogs import split_samples, optimize_splits
+
+
+"""
+This script shows you how to optimize bin edges to get equal S/N.
+The example used is a fake data set composed of measurements of 
+richness that follow an exponential distribution.
+
+"""
+
+# min and max richness
+richness_bounds = [5,100]
+a,b = richness_bounds
+np.random.seed(100)
+
+# probability function is assumed to be exponential
+x0 = 10.  # the knee of the exponential
+norm = 6.065 # this norm was explicitly calculated for x0=10 and richness bounds [5,10]
+prob = lambda x: np.exp(-x/x0)/norm # analytical pdf of exponential distribution
+cdf = lambda x : np.piecewise(x,[x<=a,np.logical_and(x>a,x<b),x>=b],[lambda x: 0.,lambda x: x0*(np.exp(-a/x0)-np.exp(-x/x0))/norm,lambda x:1.]) # analytic cdf of the same
+
+# check cdf and norm
+prange = np.linspace(a,b,100)
+pl = io.Plotter(labelX="richness $x$",labelY="$P(X<x)$")
+pl.add(prange,cdf(prange))
+pl.done("cdf.png")
+print ("Norm: ", np.trapz(prob(prange),prange))
+
+
+# sample using the inverse transform sampling method
+inv_cdf = lambda y: -x0*np.log(np.exp(-a/x0)-y*norm/x0) # analytic inverse of cdf
+Nsamples = 8000
+samples = [inv_cdf(np.random.random()) for x in range(Nsamples)]
+
+
+# histogram
+nbins = 20
+n, bins, patches = plt.hist(samples, nbins, facecolor='blue', alpha=0.5,normed=True)
+plt.plot(prange,prob(prange))
+plt.xlabel("richness $X$")
+plt.ylabel("P(X)")
+
+
+
+
+
+# start with percentile splitting
+p = 4  # 4 bins
+psplit = 100./p
+plist = [psplit*i for i in range(1,p)]
+pn = np.percentile(samples,plist).tolist()
+in_split_pts = [a]+pn+[b] # the bin edges we start with
+
+# calculate the S/N in each bin
+sns,means,Ns = split_samples(samples,in_split_pts)
+print ("=== Percentile splitting ===")
+print ("S/N: ", sns)
+print ("Avg. richness: ", means)
+print ("N:", Ns)
+
+
+ 
+# === OPTIMIZATION ====        
+split_pts = optimize_splits(samples,in_split_pts)
+print ("Recommended splitting : ", split_pts)
+# =====================
+
+for split_pt in split_pts:
+    plt.axvline(x=split_pt,color="k")
+
+s_mean = (np.mean(samples))
+s_median = (np.median(samples))
+    
+plt.axvline(x=s_mean,ls="--",color="C2")
+plt.axvline(x=s_median,ls="--",color="C3")
+
+    
+
+plt.savefig("prob.png")
+        
+
+# calculate the S/N in optimized binning
+sns,means,Ns = split_samples(samples,split_pts)
+print ("=== Optimized splitting ===")
+print ("S/N: ", sns)
+print ("Avg. richness: ", means)
+print ("N:", Ns)
