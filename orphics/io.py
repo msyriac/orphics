dd4fa50ed07a17683038f7f73dabf84031b861fe
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-12-21 21:47:05 -0500

    peakaboo api adjustments

--- orphics/io.py
+++ orphics/io.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import os,sys
+import os,sys,logging,time
 from orphics import mpi
 import contextlib
 
@@ -21,6 +21,40 @@ def nostdout():
     yield
     sys.stdout = save_stdout
 
+
+## LOGGING
+
+class LoggerWriter:
+    def __init__(self, level):
+        # self.level is really like using log.debug(message)
+        # at least in my case
+        self.level = level
+
+    def write(self, message):
+        # if statement reduces the amount of newlines that are
+        # printed to the logger
+        if message != '\n':
+            self.level(message)
+
+    def flush(self):
+        # create a flush method so things can be flushed when
+        # the system wants to. Not sure if simply 'printing'
+        # sys.stderr is the correct way to do it, but it seemed
+        # to work properly for me.
+        self.level(sys.stderr)
+
+    
+def get_logger(logname)        :
+    logging.basicConfig(filename=logname+str(time.time()*10)+".log",level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M',filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    logger = logging.getLogger('')
+    sys.stdout = LoggerWriter(logger.debug)
+    sys.stderr = LoggerWriter(logger.warning)
+    return logger    
     
 ### FILE I/O
     
