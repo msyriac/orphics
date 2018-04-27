64792ff67cdb3d4ca77987a5cba2487e294e80de
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-04-27 17:39:07 -0400

    default theory

--- orphics/cosmology.py
+++ orphics/cosmology.py
@@ -13,7 +13,7 @@ try:
 except:
     import pickle
 
-import time, re
+import time, re, os
 from scipy.integrate import odeint
 
 defaultConstants = {'TCMB': 2.7255
@@ -833,7 +833,11 @@ def validateMapType(mapXYType):
       bcolors.FAIL+"\""+mapXYType+"\" is an invalid map type. XY must be a two" + \
       " letter combination of T, E and B. e.g TT or TE."+bcolors.ENDC
 
-        
+
+def default_theory():
+    cambRoot = os.path.dirname(__file__)+"/../data/Aug6_highAcc_CDM"
+    return loadTheorySpectraFromCAMB(cambRoot,unlensedEqualsLensed=False,useTotal=False,TCMB = 2.7255e6,lpad=9000,get_dimensionless=False)
+    
 def loadTheorySpectraFromCAMB(cambRoot,unlensedEqualsLensed=False,useTotal=False,TCMB = 2.7255e6,lpad=9000,get_dimensionless=True):
     '''
     Given a CAMB path+output_root, reads CMB and lensing Cls into 
