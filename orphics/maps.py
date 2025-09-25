fd4f5bf692230cb10c20dfe8005098c3ba9321d5
Mathew Madhavacheril <mathewsyriac@gmail.com>
2025-09-25 14:51:10 -0400

    fix bug in aperture photometry background subtraction

--- orphics/maps.py
+++ orphics/maps.py
@@ -2443,10 +2443,10 @@ def flux(thumbs,aperture_radius,annulus_width=None,modrmap=None,pixsizemap=None)
     if modrmap is None: modrmap = thumbs.modrmap()
     if annulus_width is None: annulus_width = (np.sqrt(2.)-1.) * aperture_radius
     # Get the mean background level from the annulus
-    mean = thumbs[...,np.logical_and(modrmap>aperture_radius,modrmap<(aperture_radius+annulus_width))].mean()
+    mean = thumbs[...,np.logical_and(modrmap>aperture_radius,modrmap<(aperture_radius+annulus_width))].mean(axis=-1)
     if pixsizemap is None: pixsizemap = thumbs.pixsizemap()
     # Subtract the mean, multiply by pixel areas and sum
-    return (((thumbs-mean)*pixsizemap)[...,modrmap<=aperture_radius]).sum(axis=-1)
+    return (((thumbs-mean[:,None,None])*pixsizemap)[...,modrmap<=aperture_radius]).sum(axis=-1)
 
 
 
